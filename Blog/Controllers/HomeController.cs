161603823b2405d7f4d1e047d57d805efa2d043a
denserel <Denzerel@gmail.com>
2022-01-16 13:35:08 +0100

    Add project files.

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public HomeController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit ()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Blog/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
